688806df66d9c31c9d20f982e3da91d7f0b06cb1
roiDaniela <roi.fogler@gmail.com>
2018-03-15 20:38:42 +0200

    fix

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 var reservations = {
   'Bob': { claimed: false },
-  'Ted': { claimed: true }
+  'Ted': { claimed: true },
+  'Roi': {  }
 }
 
 var name = prompt('Please enter the name for your reservation');
 
 var claimReservation = function () {
-  // write your code here!
+  
+  if(reservations[name]){
+    // If the reservation exists and is unclaimed, welcome the user (use alert).
+    if(!reservations[name].claimed){
+      alert("welcome " + name + "!");
+    }    
+    // If the reservation exists and is already claimed, inform the user about the situation (use alert).
+    else{
+      alert("already claimed");
+    }
+  }
+  // If there is no reservation, tell the user there is nothing under their name (user alert).
+  else{
+    alert("There is nothing under name - " + name);
+  }
 }
 
 claimReservation();
